663d712c87d8061bf7b5078812da6ab5a3af5e50
BillyPu <yinpsoft@vip.qq.com>
2021-07-22 01:30:04 +0800

    new file:   dep/assimp-3.1.1.zip 	modified:   gl_model/main.cpp 	modified:   include/assimp/Compiler/poppack1.h 	modified:   include/assimp/Compiler/pstdint.h 	modified:   include/assimp/Compiler/pushpack1.h 	modified:   include/assimp/DefaultLogger.hpp 	modified:   include/assimp/Exporter.hpp 	modified:   include/assimp/IOStream.hpp 	modified:   include/assimp/IOSystem.hpp 	modified:   include/assimp/Importer.hpp 	modified:   include/assimp/LogStream.hpp 	modified:   include/assimp/Logger.hpp 	modified:   include/assimp/NullLogger.hpp 	modified:   include/assimp/ProgressHandler.hpp 	modified:   include/assimp/ai_assert.h 	modified:   include/assimp/anim.h 	modified:   include/assimp/camera.h 	modified:   include/assimp/cfileio.h 	modified:   include/assimp/cimport.h 	modified:   include/assimp/color4.h 	modified:   include/assimp/color4.inl 	modified:   include/assimp/config.h 	modified:   include/assimp/defs.h 	modified:   include/assimp/importerdesc.h 	modified:   include/assimp/light.h 	modified:   include/assimp/material.h 	modified:   include/assimp/material.inl 	modified:   include/assimp/matrix3x3.h 	modified:   include/assimp/matrix3x3.inl 	modified:   include/assimp/matrix4x4.h 	modified:   include/assimp/matrix4x4.inl 	modified:   include/assimp/mesh.h 	modified:   include/assimp/postprocess.h 	modified:   include/assimp/quaternion.h 	modified:   include/assimp/quaternion.inl 	modified:   include/assimp/scene.h 	modified:   include/assimp/texture.h 	modified:   include/assimp/types.h 	modified:   include/assimp/vector2.h 	modified:   include/assimp/vector2.inl 	modified:   include/assimp/vector3.h 	modified:   include/assimp/vector3.inl 	modified:   include/assimp/version.h 	new file:   lib/macos/libassimp.3.1.1.dylib 	new file:   lib/macos/libassimp.3.dylib 	modified:   lib/macos/libassimp.dylib

--- gl_model/main.cpp
+++ gl_model/main.cpp
@@ -41,7 +41,7 @@ int main() {
         (solution_base_path + "assets/shaders/multilights.object.vs").c_str(), 
         (solution_base_path + "assets/shaders/multilights.object.fs").c_str());
 
-    GLModel our_model(solution_base_path + "assets/meshes/nanosuit/nanosuit.obj");
+    GLModel our_model(solution_base_path + "assets/meshes/cyborg/cyborg.obj");
 
     glm::mat4 model = glm::mat4(1.0f);
     glm::vec3 trans_mat = glm::vec3(0.0f, 0.0f, 0.0f);
