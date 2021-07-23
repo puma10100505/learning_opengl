f07fb1a8e0be069a7812608491a1b9bb5ff361ca
billypu <puma_tw1@126.com>
2021-07-23 09:11:46 +0800

    mod

--- include/CommonDefines.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#define LearningOpenGL_VERSION_MAJOR 1
-#define LearningOpenGL_VERSION_MINOR 0
-
-const std::string solution_base_path = "/Users/yinp/devspace/playground/learning_opengl/";
